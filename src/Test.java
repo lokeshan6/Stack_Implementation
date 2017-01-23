b6b793516bbfc22f5a1681755a0b644fff4a8946
Lokesh Shanmuganandam <lokeshshanmuganandam@Lokeshs-MBP.fios-router.home>
2017-01-23 17:28:45 -0500

    Final commit stack implementation

---
+++ src/Test.java
@@ -0,0 +1,21 @@
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		MyStack<String> stack = new MyStack<String>();
+		stack.push("aa");
+		stack.push("bb");
+		stack.push("ca");
+//		stack.push(4);
+//		stack.push(5);
+//		stack.push(6);
+//		stack.push(7);
+//		stack.push(8);
+		System.out.println(stack.peek());
+		
+		
+
+	}
+
+}
